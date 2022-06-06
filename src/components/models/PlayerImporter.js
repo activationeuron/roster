eb862dadc8da26d90ff691dd77f675da9c364a34
activationeuron <aforamitrai@gmail.com>
2022-06-06 14:17:13 +0530

    roster impoter select file style

--- src/components/models/PlayerImporter.js
+++ src/components/models/PlayerImporter.js
@@ -13,49 +13,53 @@ function PlayerImporter({ uiDispatch, showImporter }) {
     <>
       <div className='model   flex justify-center  '>
         <div className='w-[600px] h-[600px] bg-[#383838] p-10 relative  rounded-xl shadow-xl text-white'>
-          <div className='flex justify-between border-b-2  py-2'>
+          <div className='flex justify-between border-color border-b-2  py-2'>
             <div className='font-bold tracking-wider'>Importer</div>
             <div
               onClick={() =>
                 uiDispatch({ type: 'TOGGLE_IMPORTER', payload: !showImporter })
               }
+              className='cursor-pointer'
             >
               <Close />
             </div>
           </div>
           <div>
-            <label className='block my-5'>
-              <input
-                type='file'
-                name='file'
-                accept='.csv'
-                class='text-sm file:mr-5 file:py-2 file:px-6 file:rounded-lg file:border-0 file:text-sm file:font-medium'
-                onChange={(e) => handleFileSelect(e)(rosterDispatcher)}
-              />
-            </label>
+            <div className='py-2'>Roster File</div>
+            <label for='files' className='block my-5'></label>
 
+            <input
+              type='file'
+              name='file'
+              id='files'
+              accept='.csv'
+              class={
+                'text-sm  rounded-xl ile:mr-5 file:py-2 file:px-6 file:rounded-lg file:border-0 file:text-sm file:font-medium border-2 ' +
+                [csvErr.length ? 'border-red-500' : 'border-color']
+              }
+              onChange={(e) => handleFileSelect(e)(rosterDispatcher)}
+            />
             {!csvErr.length ? (
               <div className='text-sm '>
                 {!players.length ? (
-                  <div className='text-gray-500'>
+                  <div className='text-gray-500 mx-2 py-2'>
                     {' '}
                     File Must be in .csv format
                   </div>
                 ) : (
-                  <div className='text-gray-100 font-medium'>
+                  <div className='text-gray-100 font-medium py-2'>
                     Current File Summery
                   </div>
                 )}
               </div>
             ) : (
-              <div className='text-red-500'>
+              <div className='text-red-500 py-2 px-2'>
                 <div className=''>Error </div>
                 {csvErr.slice(0, 4).map((error) => {
                   return <div className='text-sm'>{error}</div>;
                 })}
               </div>
             )}
-
             <div className=''>
               {players.length ? (
                 <table class=' '>
